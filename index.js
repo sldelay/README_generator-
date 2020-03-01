a197a03af6e93a172f6f5f98ffe52371ce2248ec
Sam Delay <samuel.delay@gmail.com>
2020-03-01 13:27:55 -0500

    added email question and prompt conditionals

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const axios = require("axios")
+const axios = require("./.gitignore/node_modules/axios");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -11,6 +11,11 @@ function promptUser() {
             type: "input",
             name: "username",
             message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email?"
         },
         {
             type: "input",
@@ -32,12 +37,15 @@ function promptUser() {
             name: "installationConfirm",
             message: "Is there an installation process?"
         }, {
-            when: function (response) {
-                return response;
-            },
             type: "input",
             name: "installation",
-            message: "What is the installation process?"
+            message: "What is the installation process?",
+            when: function (response) {
+                if (response.installationConfirm === false) {
+                    response.installation = "No installation required!";
+                }
+                return response.installationConfirm;
+            },
         },
         {
             type: "input",
@@ -55,47 +63,33 @@ function promptUser() {
             name: "contributeConfirm",
             message: "Are you interested in other developers contributing to your project?"
         }, {
-            when: function (response) {
-                return response;
-            },
             type: "input",
             name: "contribute",
-            message: "How would you like contributions to be made?"
+            message: "How would you like contributions to be made?",
+            when: function (response) {
+                if (response.contributeConfirm === false) {
+                    response.contribute = "No contributions are being accepted at this time.";
+                }
+                return response.contributeConfirm;
+            },
         },
         {
             type: "confirm",
             name: "testingConfirm",
             message: "Does this application require any testing?"
         }, {
-            when: function (response) {
-                return response;
-            },
             type: "input",
             name: "testing",
-            message: "Please explain how to run the tests"
+            message: "Please explain how to run the tests",
+            when: function (response) {
+                if (response.testingConfirm === false) {
+                    response.testing = "No testing required!";
+                }
+                return response.testingConfirm;
+            },
         }
     ]);
 }
-
-// let email;
-// let profileImg;
-
-// function githubGet(answers) {
-//     const username = answers.username;
-//     const queryUrl = `https://api.github.com/users/${username}`;
-
-//     axios.get(queryUrl).then((res) => {
-//         profileImg = res.data.avatar_url;
-//         email = res.data.email;
-//         return profileImg, email
-//     })
-
-//     return {
-//         email: email,
-//         img: profileImg
-//        }
-
-// };
 
 function generateReadMe(answers, gitObj) {
     return `
@@ -121,7 +115,7 @@ ${answers.contribute}
 ## Testing
 ${answers.testing}
 ## Questions
-${gitObj.contact}
+Email: ${answers.email}
 ![alt txt](${gitObj.img})
     `
 }
@@ -137,14 +131,10 @@ async function init() {
 
         const gitObj = await axios.get(queryUrl).then((res) => {
             profileImg = res.data.avatar_url;
-            email = res.data.email;
             return {
-                contact: email,
                 img: profileImg
             }
-        })
-
-        // const gitObj = await githubGet(answers);
+        });
 
         const README = await generateReadMe(answers, gitObj);
 
